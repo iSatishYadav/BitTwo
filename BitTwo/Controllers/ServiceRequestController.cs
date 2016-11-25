476fc45d992528d77aca1e74f19e9cae2571decd
Satish <monstersatish@live.com>
2016-11-25 19:43:17 +0530

    init

---
+++ BitTwo/Controllers/ServiceRequestController.cs
@@ -0,0 +1,37 @@
+﻿using BitTwo.Models;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.FormFlow;
+using Microsoft.Bot.Connector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace BitTwo.Controllers
+{
+    public class ServiceRequestController : ApiController
+    {
+        internal static IDialog<ServiceRequest> MakeServiceRequestDialog()
+        {
+            return Chain.From(
+                () => FormDialog.FromForm(ServiceRequest.CreateServiceRequestForm)
+                );
+        }
+
+        public virtual async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        {
+            if(activity != null && activity.GetActivityType() == ActivityTypes.Message)
+            {
+                await Conversation.SendAsync(activity, MakeServiceRequestDialog);
+                return new HttpResponseMessage(HttpStatusCode.OK);
+            }
+            else
+            {
+                return new HttpResponseMessage(HttpStatusCode.NotFound);
+            }
+        }
+    }
+}
---
+++ BitTwo/Models/ServiceRequest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Bot.Builder.FormFlow;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BitTwo.Models
+{
+    public enum RequestType
+    {
+        Software, Hardware, Network, Others
+    }
+
+    [Serializable]
+    public class ServiceRequest
+    {
+        public RequestType Type { get; set; }
+        public string Description { get; set; }
+
+        public static IForm<ServiceRequest> CreateServiceRequestForm()
+        {
+            return new FormBuilder<ServiceRequest>()
+                .Message("Welcome to BitTwo")
+                .Build();
+        }
+    }
+}
